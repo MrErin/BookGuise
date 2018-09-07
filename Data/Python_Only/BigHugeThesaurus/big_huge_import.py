5f9257829123ad2cc8433eee2984b815030d99e1
MrErin <erinmeaker@gmail.com>
2018-09-07 11:31:22 -0500

    working on language import

---
+++ Data/Python_Only/BigHugeThesaurus/big_huge_import.py
@@ -0,0 +1,43 @@
+# This import needs to accept input of a set of keywords and loop through them, for each one turning back a list of synonyms. This list needs to be accumulated and then used as search terms for the books in the database.
+# Since I want the result set to be unique, it should be a set.
+
+import json
+import requests
+
+
+with open('Data/DataFiles/config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+keywords = ["love", "circuses", "the maze"]
+dev_key = config['Big_Huge_Thesaurus_key']
+
+# for current_word in keywords:
+# TODO: handle spaces and special characters per keyword here.
+# TODO: Also, how does the API handle plurals?
+
+# bht_request = "http://words.bighugelabs.com/api/2/{0}/{1}/json".format(
+#     dev_key, current_word)
+# print(bht_request)
+# response = requests.get(bht_request)
+# response.json()
+bht_request = 'Data/Python_Only/BigHugeThesaurus/sample.json'
+
+adjective_synonyms = set()
+noun_synonyms = set()
+verb_synonyms = set()
+
+with open(bht_request) as request:
+    data = json.load(request)
+
+    if "adjective" in data:
+        for synonym in data["adjective"]["syn"]:
+            adjective_synonyms.add(synonym)
+    if "noun" in data:
+        for synonym in data["noun"]["syn"]:
+            noun_synonyms.add(synonym)
+    if "verb" in data:
+        for synonym in data["verb"]["syn"]:
+            verb_synonyms.add(synonym)
+    print("Adjectives: ", adjective_synonyms)
+    print("Nouns: ", noun_synonyms)
+    print("Verbs: ", verb_synonyms)
--- Data/Python_Only/BigHugeThesaurus/readme.md
+++ Data/Python_Only/BigHugeThesaurus/readme.md
@@ -1,3 +1,8 @@
 https://words.bighugelabs.com/api.php
 
 https://pypi.org/project/pyhugeconnector/
+
+
+Sample URL: 
+
+http://words.bighugelabs.com/api/2/{key}/{word}/json
