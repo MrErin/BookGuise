a899ac82b20bdf469bc440e7108cf49d2a872520
MrErin <erinmeaker@gmail.com>
2018-09-13 15:49:45 -0500

    working on librarything import

--- bookguise/suggested_book_builder.py
+++ bookguise/suggested_book_builder.py
@@ -2,7 +2,8 @@ import xml.etree.ElementTree as ET
 import json
 import requests
 import traceback
-from .keyword_exclusion_list import exclusion_keywords as exclusions
+# need to put a period before "keyword exclusion list" when ready to use this in the app again.
+from keyword_exclusion_list import exclusion_keywords as exclusions
 
 
 class Suggested_Book():
@@ -13,30 +14,28 @@ class Suggested_Book():
             config = json.load(config_file)
 
         gr_dev_key = config['Goodreads_key']
-        lt_dev_key = config['LibraryThing_key']
 
         title_format = book_title.replace(' ', '+')
         goodreads_request = "https://www.goodreads.com/book/title.xml?&key={0}&title={1}".format(
             gr_dev_key, title_format)
-        librarything_request = ''
 
         try:
-            response = requests.get(goodreads_request)
-            root = ET.fromstring(response.content)
-            book_root = root.find("book")
+            gr_response = requests.get(goodreads_request)
+            gr_root = ET.fromstring(gr_response.content)
+            gr_book_root = gr_root.find("book")
 
-            self.gr_id = book_root.find("id").text
-            if not book_root.find("isbn") is None:
-                self.isbn = book_root.find("isbn").text
-            if not book_root.find("isbn13") is None:
-                self.isbn13 = book_root.find("isbn13").text
-            self.title = book_root.find("title").text
-            self.publication_year = book_root.find(
+            self.gr_id = gr_book_root.find("id").text
+            if not gr_book_root.find("isbn") is None:
+                self.isbn = gr_book_root.find("isbn").text
+            if not gr_book_root.find("isbn13") is None:
+                self.isbn13 = gr_book_root.find("isbn13").text
+            self.title = gr_book_root.find("title").text
+            self.publication_year = gr_book_root.find(
                 "publication_year").text
-            self.gr_link = book_root.find("link").text
+            self.gr_link = gr_book_root.find("link").text
             self.keywords = list()
-            if not book_root.find("popular_shelves") is None:
-                for shelf in book_root.find("popular_shelves").iter("shelf"):
+            if not gr_book_root.find("popular_shelves") is None:
+                for shelf in gr_book_root.find("popular_shelves").iter("shelf"):
                     if shelf.attrib["name"] not in exclusions:
                         try:
                             shelf.attrib["name"].encode('ascii')
@@ -47,16 +46,32 @@ class Suggested_Book():
                         except UnicodeEncodeError:
                             pass
 
-            if __name__ == '__main__':
-                print(goodreads_request)
-                print(librarything_request)
+            lt_dev_key = config['LibraryThing_key']
+            librarything_request = 'https://www.librarything.com/services/rest/1.1/?method=librarything.ck.getwork&isbn={0}&apikey={1}'.format(
+                self.isbn, lt_dev_key)
+
+            lt_response = requests.get(librarything_request)
+            lt_root = ET.fromstring(lt_response.content)
+            lt_book_root = lt_root.tag("ltml")
+            self.lt_id = lt_root.attrib["version"]
+
+            # self.lt_id = lt_book_root.find("item").attrib["id"]
 
         except:
             print('The goodreads request was: ', goodreads_request)
             print('The librarything request was: ', librarything_request)
-
             traceback.print_exc()
+
+    def __repr__(self):
+        # print('Goodreads ID: ', self.gr_id)
+        # print('ISBN: ', self.isbn)
+        # print('ISBN13: ', self.isbn13)
+        # print('Title: ', self.title)
+        # print('Publication Year: ', self.publication_year)
+        # print('Goodreads Link: ', self.gr_link)
+        # print('Keywords: ', self.keywords)
+        print('LibraryThing ID: ', self.lt_id)
 
 
 if __name__ == '__main__':
-    Suggested_Book('Harry Potter')
+    print(Suggested_Book('Harry Potter'))
